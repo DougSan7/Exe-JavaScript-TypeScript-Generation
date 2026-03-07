add7833ffe45b5c6d5e1eee21812988fcf8e53f5
Douglas <douglassan666@hotmail.com>
2026-03-06 23:55:08 -0300

    commit

--- modulo 8  - TypeScript-Collections/atividade04.ts
+++ modulo 8  - TypeScript-Collections/atividade04.ts
@@ -4,15 +4,13 @@ import readlinesync = require('readline-sync');
 const setLista = new Set<number>([2,5,1,3,4,9,7,8,10,6]);
 let numero = 0;
 let localizado;
-
+let cont = 0;
 console.log("\n**********Seu numero está aqui ???************");
 numero = readlinesync.questionInt("Digite o numero que voce deseja encontrar: ");
 
 
- for (let i of setLista){
-  if(setLista.has(numero) == true){
-    localizado = true;
-  }
+if(setLista.has(numero) == true){
+  localizado = true;
 }
 
 if(localizado == true){
